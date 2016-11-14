5cd27756652155c7ef774cd58a6187ad1549ad91
Bernat Orellana <borellan@xtec.cat>
2016-11-14 15:50:02 +0100

    exercici preus treatre

---
+++ UF2/20161114/TarifesTeatre/src/tarifesteatre/TarifesTeatre.java
@@ -0,0 +1,54 @@
+
+package tarifesteatre;
+
+/**
+ *
+ * @author BERNAT
+ */
+public class TarifesTeatre {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    /*
+    1 (platea) : 60?
+2 (laterals) : 50?
+3 (primer  pis): 40?
+4 (segon pis): 30?
+
+    */
+    public enum TipusSeient {
+        PLATEA(60),
+        LATERAL(50),
+        PRIMER_PIS(40),
+        SEGON_PIS(30);
+        
+        private double mPreu;
+        TipusSeient (double preu) {
+            mPreu = preu;
+        }
+        public double getPreu(){ return mPreu;}
+
+    }
+    
+    public double getPreu(TipusSeient tipus, int edat) {
+        //<4 anys: no paga
+        if(edat<4) return 0;
+        // [4-12) anys: 25% descompte sobre el seient.
+        if(edat<12) return tipus.getPreu()*0.75;
+        
+        //[12-60): tarifa normal
+        if(edat<60) return tipus.getPreu() ;
+
+        //edat>=60: 50% descompte a platea, la resta al 80% de descompte.
+        if(tipus==TipusSeient.PLATEA) 
+            return tipus.getPreu()*0.5;
+        else 
+            return tipus.getPreu()*0.8;
+
+    }
+    
+}
