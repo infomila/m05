22d91fb5b109d6810b7eeb6ae789fefbda5e90b3
Bernat Orellana <borellan@xtec.cat>
2017-01-23 18:05:25 +0100

    d

--- UF2/20170123_examen_2015_01_20_solucio_jmockit/UF2_Testing/test/net/iesmila/ED/UF2/fabrica/LectorTest.java
+++ UF2/20170123_examen_2015_01_20_solucio_jmockit/UF2_Testing/test/net/iesmila/ED/UF2/fabrica/LectorTest.java
@@ -1,5 +1,6 @@
 package net.iesmila.ED.UF2.fabrica;
 
+ 
 import java.util.HashMap;
 import mockit.Expectations;
 import mockit.Mock;
@@ -18,9 +19,86 @@ public class LectorTest {
     /**
      * Test of extreuParaulesAmbDefinicio method, of class Lector.
      */
+    
+    @Mocked
+    DiccionariOnline mDiccionariOnline;
+    
     @Test
     public void testInformeText() {
         
+//        
+//        //--------------------------------------------------
+//        //   Bloc MockUp 
+//        //--------------------------------------------------        
+//        new MockUp<DiccionariOnline>(){
+//
+//            /**
+//             * donada una paraula en retorna el tipus ( nom, adjectiu, verb...)
+//             */
+//            @Mock
+//            public TipusParaula getTipusParaula(String paraula) {
+//
+//                if(paraula.equals("gos")||paraula.equals("gat")||paraula.equals("casa")) {
+//                    return TipusParaula.NOM;
+//                }
+//                return TipusParaula.ARTICLE;
+//            }
+//
+//            @Mock
+//            public String getDescripcio(String paraula) {        
+//
+//                HashMap<String, String> definicions = new HashMap<String, String>();
+//                definicions.put("gos","mamifer cannid");
+//                definicions.put("gat","mamifer feli");
+//                definicions.put("casa","llar dolça llar");
+//
+//               return definicions.get(paraula);
+//
+//            }            
+//        };
+        
+        
+        //--------------------------------------------------
+        //   Bloc d'expectations ( respostes pregravades ) 
+        //--------------------------------------------------
+        new NonStrictExpectations(){{
+            
+//            mDiccionariOnline.getTipusParaula(anyString);
+//            returns ( TipusParaula.ARTICLE,     // el 
+//                      TipusParaula.NOM,         // gos
+//                      TipusParaula.CONJUNCIO,   // i
+//                      TipusParaula.ARTICLE,     //el
+//                      TipusParaula.NOM,         //gat
+//                      TipusParaula.VERB     ,   // viuen
+//                      TipusParaula.CONJUNCIO     ,// a
+//                      TipusParaula.NOM             // casa
+//                    ); 
+            
+            mDiccionariOnline.getTipusParaula("gos");
+            returns ( TipusParaula.NOM);
+            mDiccionariOnline.getTipusParaula("gat");
+            returns ( TipusParaula.NOM);
+            mDiccionariOnline.getTipusParaula("casa");
+            returns ( TipusParaula.NOM);
+            mDiccionariOnline.getTipusParaula(anyString);
+            returns ( TipusParaula.ARTICLE);
+            
+            
+//            mDiccionariOnline.getDescripcio(anyString);
+//            returns (   "mamifer cannid",
+//                        "mamifer feli",
+//                        "llar dolça llar");
+            mDiccionariOnline.getDescripcio("gos");
+            returns (   "mamifer cannid" );
+            mDiccionariOnline.getDescripcio("gat");
+            returns (   "mamifer feli" );
+            mDiccionariOnline.getDescripcio("casa");
+            returns (   "llar dolça llar" );
+        }};
+        
+        //----------------------------------------------------
+        // Cos del test
+        //----------------------------------------------------             
         Lector l = new Lector();
         String informeObtingut = l.extreuParaulesAmbDefinicio("El gos i el gat viuen a casa");
         String informeEsperat = "gos: mamifer cannid,gat: mamifer feli,casa: llar dolça llar";
