35cf5938ee82d151d2113ddc6a05edd1ff2e618f
Bernat Orellana <borellan@xtec.cat>
2017-02-22 16:02:50 +0100

    Pujada Refactoring !

---
+++ UF2/20160220_refactoring/RefactoringExampleNB_solucio_DAM/RefactoringExampleNB/src/refactoring/Customer.java
@@ -0,0 +1,54 @@
+package refactoring;
+
+
+import java.util.Iterator;
+import java.util.LinkedList;
+
+//From book: 'Refactoring' by Martin Fowler
+//This is the original code before refactoring begins
+
+public class Customer {
+
+	private String _name;
+	private LinkedList<Rental> _rentals = new LinkedList<Rental>();
+	
+	public Customer(String name) {
+		_name = name;
+	}
+	
+	public void addRental(Rental arg) {
+		_rentals.add(arg);
+	}
+	
+	public String getName() {
+		return _name;
+	}
+	
+	public String statement() {
+		double totalAmount = 0;
+		int totalPoints = 0;
+		String result = "Rental Record for " + getName() + "\n";
+		
+                for(Rental rental: _rentals) {
+                    
+                        double thisAmount = rental.getAmount();
+                   			
+			// add frequent renter points
+			totalPoints++; //
+			// add bonus for a two day new release rental
+			if ((rental.getMovie().getPriceCode() == Movie.NEW_RELEASE) && rental.getDaysRented() > 1) 
+				totalPoints++;
+			
+			// show figures for this rental
+			result += "\t" + rental.getMovie().getTitle() + "\t" + String.valueOf(thisAmount) + "\n";
+			totalAmount += thisAmount;
+		}
+		
+		// add footer lines
+		result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
+		result += "You earned " + String.valueOf(totalPoints) + " frequent renter points";
+		
+		return result;
+	}
+
+}
